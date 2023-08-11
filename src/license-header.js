19211d485d15d6410f6f1fb28b9995cfac8a1027
Aonrud <aonrud@fastmail.com>
2023-08-11 12:25:57 +0100

    chore: update licence header year

--- src/license-header.js
+++ src/license-header.js
@@ -1,5 +1,5 @@
 const header = `/*! Timeline
- *Copyright (C) 2021 Aonghus Storey
+ *Copyright (C) 2021-${new Date().getFullYear()} Aonghus Storey
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
